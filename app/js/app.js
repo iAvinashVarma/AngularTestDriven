6ef97113236d9f88eb9b8dac2e6f1b55bbec4a66
Avinash Varma <iavinashvarma@gmail.com>
2018-01-14 13:54:11 +0530

    Adding materialize and new test case.

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,19 @@ var testingApp = angular.module('testingApp', []);
 
 testingApp.controller('aviCtrl', function($rootScope, $scope){
     $scope.title = "Testing AngularJS Applications.";
+    $scope.destinations = [];
+
+    $scope.newDestination = {
+        city: undefined,
+        country: undefined
+    };
+
+    $scope.addDestination = function() {
+        $scope.destinations.push(
+            {
+                city: $scope.newDestination.city,
+                country: $scope.newDestination.country
+            }
+        );
+    };
 });
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -17,6 +17,30 @@ describe('Testing AngularJS Test Suite', function(){
             expect(scope.title).toBeDefined();
             expect(scope.title).toBe("Testing AngularJS Applications.");
         })
+
+        it('should add two destinations to the destination list', function() {
+            expect(scope.destinations).toBeDefined();
+            expect(scope.destinations.length).toBe(0);
+            scope.newDestination = {
+                city: "London",
+                country: "England"
+            }
+            
+            scope.addDestination();
+            
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+            
+            scope.newDestination.city = "Frankfurt";
+            scope.newDestination.country = "Germany";
+            
+            scope.addDestination();
+            
+             expect(scope.destinations.length).toBe(2);
+            expect(scope.destinations[1].city).toBe("Frankfurt");
+            expect(scope.destinations[1].country).toBe("Germany");
+        })
         
     });
 });
