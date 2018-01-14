6ed407767e781b4c133937866ab01f5ae324e8bf
Avinash Varma <iavinashvarma@gmail.com>
2018-01-14 12:04:03 +0530

    Adding beforeEach and afterEach.

--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -1,16 +1,19 @@
 describe('Testing AngularJS Test Suite', function(){
+
+    beforeEach(module('testingApp'));
+
     describe('Testing AngularJS Controller', function(){
-        
+        var scope, ctrl;
+        beforeEach(inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            ctrl = $controller('aviCtrl', { $scope: scope });
+        }));
+
+        afterEach(function(){
+            // Cleanup Code
+        });
+
         it('should initialize the title in the scope', function(){
-            module('testingApp');
-            
-            var scope = {};
-            var ctrl;
-            
-            inject(function($controller){
-               ctrl = $controller('aviCtrl', {$scope:scope});
-            });
-            
             expect(scope.title).toBeDefined();
             expect(scope.title).toBe("Testing AngularJS Applications.");
         })
